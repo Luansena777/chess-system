37c209ed8612af72879c250704914d619aa587b1
Luan Sena da Silva <luan.sena1507@gmail.com>
2025-08-11 21:45:34 -0300

    Moving pieces

--- src/UI/UI.java
+++ src/UI/UI.java
@@ -1,7 +1,11 @@
 package UI;
 
 import chess.ChessPiece;
+import chess.ChessPosition;
 import chess.Color;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
 
 public class UI {
     // https://stackoverflow.com/questions/5762491/how-to-print-color-in-console-using-system-out-println
@@ -24,6 +28,16 @@ public class UI {
     public static final String ANSI_CYAN_BACKGROUND = "\u001B[46m";
     public static final String ANSI_WHITE_BACKGROUND = "\u001B[47m";
 
+    public static ChessPosition readChessPosition(Scanner scan) {
+        try {
+            String s = scan.nextLine();
+            char column = s.charAt(0);
+            int row = Integer.parseInt(s.substring(1));
+            return new ChessPosition(column, row);
+        } catch (RuntimeException e) {
+            throw new InputMismatchException("Erro ao ler ChessPosition. Os valores válidos são de a1 a h8.");
+        }
+    }
 
     /**
      * Imprime o tabuleiro no console.
@@ -47,9 +61,9 @@ public class UI {
             System.out.print("-");
         } else {
             if (piece.getColor() == Color.WHITE) {
-                System.out.println(ANSI_WHITE + piece + ANSI_RESET);
+                System.out.print(ANSI_BLUE + piece + ANSI_RESET);
             } else {
-                System.out.println(ANSI_YELLOW + piece + ANSI_RESET);
+                System.out.print(ANSI_YELLOW + piece + ANSI_RESET);
             }
         }
         System.out.print(" ");
